87ba68eb000a1e1eacdce2d4badcaee52db41572
skroneis <skroneis@yahoo.com>
2017-08-28 16:20:18 +0200

    :camel:

--- send.js
+++ send.js
@@ -1,5 +1,8 @@
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+var async = require('async');
+//config
+var config = require('./config');
 
 // =============================
 // UDP client ==================
@@ -8,12 +11,26 @@ var server = dgram.createSocket('udp4');
 var PORT = 64301; //64345;
 var HOST = '192.168.0.116'; //'255.255.255.255';
 
-server.bind( function() {server.setBroadcast(true)});
+// console.log(config.UDPHosts[0]);
+
+server.bind(function () { server.setBroadcast(true) });
 //server.bind();
 
-var actuals = {sepp: "hugo"};
-server.send(JSON.stringify(actuals), PORT, HOST, (err) => {
-    console.log("sending...");
-    server.close();
+var actuals = { sepp: "hugo" };
+
+server.on('listening', () => {
+    async.each(config.UDPHosts, function (host, callback) {
+        // console.log('HOST: ' + host);
+        server.send(JSON.stringify(actuals), PORT, host, (err) => {
+            console.log("sending... to " + host);
+        });
+        callback();
+    }, function (err) {
+        if (err)
+            console.log(err);
+    });
 });
+
+
+
 
